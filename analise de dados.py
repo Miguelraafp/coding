5b39fdca21ebfe754a80bb3392a9a4d7da6a4e86
migueraafp <miguelraaf@gmail.com>
2024-11-19 19:06:51 -0300

    1° projeto no github

---
+++ analise de dados.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import plotly.express as px
+
+lista_arquivos = os.listdir(r"C:\Users\usuario\PycharmProjects\gitcodes\Vendas")
+tabela_total = pd.DataFrame()
+
+for arquivos in lista_arquivos:
+    if 'Vendas' in arquivos:
+        tabela = pd.read_csv(fr"C:\Users\usuario\PycharmProjects\gitcodes\Vendas\{arquivos}")
+        tabela_total = pd.concat([tabela_total, tabela])  # Corrigido aqui
+
+tabela_produtos = tabela_total.groupby('Produto').sum()
+tabela_produtos = tabela_produtos[['Quantidade Vendida']].sort_values(by='Quantidade Vendida', ascending=False)
+
+
+tabela_total['Faturamento'] = tabela_total['Quantidade Vendida'] * tabela_total['Preco Unitario']
+tabela_faturamento = tabela_total.groupby('Produto').sum()
+
+tabela_faturamento = tabela_faturamento[['Faturamento']].sort_values(by='Faturamento', ascending=False)
+#print(tabela_faturamento)
+
+tabela_lojas = tabela_total.groupby('Loja').sum()
+tabela_lojas = tabela_lojas[['Faturamento']]
+#print(tabela_lojas)
+
+
+grafico = px.bar(tabela_lojas.reset_index(), x='Loja', y='Faturamento')
+grafico.show()
