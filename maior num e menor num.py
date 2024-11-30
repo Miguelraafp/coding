ef2fbff84794fd34c2254ae616bc512d3c859b0c
migueraafp <miguelraaf@gmail.com>
2024-11-30 16:05:20 -0300

    1° projeto no github

--- maior num e menor num.py
+++ maior num e menor num.py
@@ -1,11 +1,14 @@
-
-
 lista = []
-
+cont = 0
 num = 0
-while num < 10:
+resp = 'S'
+while resp == 'S':
     num = int(input("Digite um número: "))
+    cont += 1
     lista.append(num)
+    resp = str(input("Deseja continuar? [S/N] ")).upper()
+media = sum(lista) / cont
+print(f"A média dos números digitados é {media:.2f}")
+print(f"O maior número foi {max(lista)} e o menor foi {min(lista)}")
 
-print(f"O maior número dessa lista é {max(lista)} e o menor é {min(lista)}")
 
